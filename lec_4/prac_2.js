bd9d80b5bdd4c15b3fb005b155fa418010e0aa8b
Roman Strutskyi <rstruckij6@gmail.com>
2026-07-15 03:29:38 +0300

    pr2

---
+++ lec_4/prac_2.js
@@ -0,0 +1,9 @@
+let myBirthday = 7
+let myString = `Мой день рождения: ${myBirthday} числа`;
+console.log(myString);
+
+let myBirthday2
+myBirthday2 = 7
+const symbol = "май"
+let myString2 = `Мой день рождения: ${myBirthday2} числа ${symbol} месяца`;
+console.log(myString2);
